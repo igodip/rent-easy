4fc792deed78c2c9f42ea1a88b808c0192e2e356
Igor Di Paolo <igor.di.paolo@gmail.com>
2021-10-03 21:32:59 +0400

    Added logo

--- src/Header.js
+++ src/Header.js
@@ -12,7 +12,7 @@ function Header() {
             <Link to='/'>
                 <img
                     className="header__icon"
-                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                    src="./logo.png"
                     alt=""
                 />
             </Link>
